463001cc15a7a4c1ae6347d714b50b60904c2a35
MerlinRed <knight_j_k@mail.ru>
2021-09-20 00:19:13 +0500

    refactor

--- index.js
+++ index.js
@@ -14,29 +14,28 @@ const COLORS = [
 ]
 const board = document.getElementById("board")
 
+const setColor = (event) => {
+    const color = getRandomColor(COLORS)
+    event.target.style.backgroundColor = color
+    event.target.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}, 0 0 14px ${color}, 4px 4px 9px ${color}`
+    event.target.style.filter = "blur(1px)"
+}
+const removeColor = (event) => {
+    event.target.style.backgroundColor = "#1d1d1d"
+    event.target.style.boxShadow = "0 0 2px #000"
+    event.target.style.filter = "none"
+}
+
+const getRandomColor = (colors) => {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
 for (let i = 0; i < NUMBER_OF_SQUARES; i++) {
     const square = document.createElement("div")
     square.classList.add("square")
 
-    square.addEventListener("mouseover", () => setColor(square))
-    square.addEventListener("mouseleave", () => removeColor(square))
+    square.addEventListener("mouseover", setColor)
+    square.addEventListener("mouseleave", removeColor)
 
     board?.append(square)
-}
-
-const setColor = (element) => {
-    const color = getRandomColor(COLORS)
-    element.style.backgroundColor = color
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}, 0 0 14px ${color}, 4px 4px 9px ${color}`
-    element.style.filter = "blur(1px)"
-}
-const removeColor = (element) => {
-    element.style.backgroundColor = "#1d1d1d"
-    element.style.boxShadow = "0 0 2px #000"
-    element.style.filter = "none"
-}
-
-const getRandomColor = (colorsArray) => {
-    const index = Math.floor(Math.random() * colorsArray.length)
-    return colorsArray[index]
 }
